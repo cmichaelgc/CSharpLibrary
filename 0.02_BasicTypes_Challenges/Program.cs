87e3294cd0039d613986b037c2fe03595b9d3fbe
Robby Michael <Robby Michael>
2017-03-22 12:11:48 -0400

    intitial commit

---
+++ 0.02_BasicTypes_Challenges/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Charles Michael");
+            byte age;
+            string me = "Charles";
+            string dog;
+            short birth = 1987;
+
+            dog = "bruno";
+            age = 30;
+
+            Console.WriteLine(me + " was born in {0}" + ", and is now {1}" + ".", birth, age);
+            Console.WriteLine(me + " has a dog, and his name is {0}.", dog);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ 0.03_Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Simple Demo
+            string firstName;
+            string lastName;
+            string occupation;
+
+            firstName = "Charles";
+            lastName = "Michael";
+            occupation = "looking";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            //string interpolation
+            Console.WriteLine("Name : {0}" + "\n" + "Occupation: {1}", fullName, occupation);
+
+            Console.ReadLine();
+        }
+    }
+}
