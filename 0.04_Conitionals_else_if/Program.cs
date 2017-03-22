6fa3adb2707801321ae81aa7ae9b34ae2f9407b5
Robby Michael <Robby Michael>
2017-03-22 14:45:03 -0400

    more conditionals

---
+++ 0.04_Conitionals_else_if/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.04Conditionals_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int bankAccount = 50000;
+        int debt = 13500;
+        int difference = bankAccount - debt;
+
+        Console.WriteLine("I have {0} in my bank account, and I am {1 : C} in debt", bankAccount, debt);
+
+
+        Console.ReadLine();
+        }
+    }
+}
