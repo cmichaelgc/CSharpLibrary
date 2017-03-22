c2ae46ca476b56e5a9e41911df0d0394cd87d3c9
Robby Michael <Robby Michael>
2017-03-22 10:42:57 -0400

    initial commit

---
+++ 0.02_basictypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_basictypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("******Data Types******");
+
+            //String data type
+            string name = "Kenn";
+            string teacher;
+
+            teacher = "Paul";
+
+            Console.WriteLine(name);
+            Console.WriteLine(teacher);
+            Console.ReadLine();
+        }
+    }
+}
